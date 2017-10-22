c679d8c860ffdf5d5a118111fd8c321ac61e1f0c
ronny <ronny@ronnydeMacBook-Pro.local>
2017-10-22 20:15:51 +0800

    天气预报主界面搭建及网络数据解析展示

--- app/src/main/java/com/coolweather/android/gson/AQI.java
+++ app/src/main/java/com/coolweather/android/gson/AQI.java
@@ -5,4 +5,14 @@ package com.coolweather.android.gson;
  */
 
 public class AQI {
+
+    public AQICity city;
+
+    public class AQICity {
+
+        public String api;
+
+        public String pm25;
+    }
+
 }
--- app/src/main/java/com/coolweather/android/gson/Now.java
+++ app/src/main/java/com/coolweather/android/gson/Now.java
@@ -1,8 +1,22 @@
 package com.coolweather.android.gson;
 
+import com.google.gson.annotations.SerializedName;
+
 /**
  * Created by ronny on 2017/10/22.
  */
 
 public class Now {
+
+    public String temperature;
+
+    @SerializedName("cond")
+    public More more;
+
+    public class More {
+
+        @SerializedName("txt")
+        public String info;
+
+    }
 }
